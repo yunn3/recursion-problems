a4572bcbf3bbdb8fa629e266f59d67f804525236
nonlem <nonlem7432@gmail.com>
2024-05-02 02:36:55 +0000

    add: 478-priceTable

---
+++ 478-priceTable/main.py
@@ -0,0 +1,11 @@
+price_table = {
+    "Honda Civic": 24000,
+    "Chevrolet Traverse": 30000,
+    "Toyota Camry": 25000,
+    "Subaru Outback": 27000,
+    "Tesla": 100000,
+}
+
+print(price_table["Tesla"])
+print(price_table["Toyota Camry"])
+print(price_table["Chevrolet Traverse"])
