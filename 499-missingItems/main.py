ba66a02aff2a22e256a804e8afab5e67b86e1bcb
nonlem <nonlem7432@gmail.com>
2024-05-19 00:30:04 +0000

    modified: main.py

--- 499-missingItems/main.py
+++ 499-missingItems/main.py
@@ -1,9 +1,10 @@
 def missingItems(listA: list[int], listB: list[int]) -> list[int]:
-    missing_list = []
     little_bro_map = {v: v for v in listB}
 
-    for item in listA:
-        if item not in little_bro_map:
-            missing_list.append(item)
+    # for item in listA:
+    #     if item not in little_bro_map:
+    #         missing_list.append(item)
 
-    return missing_list
+    # return missing_list
+
+    return [item for item in listA if item not in little_bro_map]
