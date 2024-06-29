60841cb8181cbd8db140df414f0f1f95a37169fb
nonlem <nonlem7432@gmail.com>
2024-06-29 09:35:25 +0900

    add: 168-insertAtTail

---
+++ advanced/168-insertAtTail/main.py
@@ -0,0 +1,81 @@
+class SinglyLinkedListNode:
+
+    def __init__(self, data: int) -> None:
+        self.data = data
+        self.next = None
+
+    def insert(self, new_node: "SinglyLinkedListNode") -> None:
+        temp_node = self.next
+        self.next = new_node
+        new_node.next = temp_node
+
+
+class SinglyLinkedList:
+
+    def __init__(self, head: SinglyLinkedListNode) -> None:
+        self.head = head
+
+    def at(self, index: int) -> SinglyLinkedListNode:
+        current_node = self.head
+
+        for i in range(index):
+            current_node = current_node.next
+            if current_node is None:
+                return None
+
+        return current_node
+
+
+def getIndexValue(head: SinglyLinkedListNode, index: int) -> int:
+    linked_list = SinglyLinkedList(head)
+    node = linked_list.at(index - 1)
+    if node is None:
+        raise IndexError()
+    return node.data
+
+
+def singlyLinkedList(arr: list) -> SinglyLinkedListNode:
+    head = SinglyLinkedListNode(arr[0])
+    prev = head
+    for data in arr[1:]:
+        prev.next = SinglyLinkedListNode(data)
+        prev = prev.next
+
+    return head
+
+
+def doubleEvenNumber(head: SinglyLinkedListNode) -> SinglyLinkedListNode:
+    index = 0
+    iterator = head
+
+    while iterator is not None:
+        current_node = iterator
+        iterator = iterator.next
+
+        if index % 2 == 0:
+            new_node = SinglyLinkedListNode(current_node.data * 2)
+            current_node.insert(new_node)
+
+        index += 1
+
+    return head
+
+
+def insertAtHead(head: SinglyLinkedListNode, data: int) -> SinglyLinkedListNode:
+    new_node = SinglyLinkedListNode(data)
+    new_node.next = head
+    head = new_node
+
+    return head
+
+
+def insertAtTail(head: SinglyLinkedListNode, data: int) -> SinglyLinkedListNode:
+    current_node = head
+
+    while current_node.next is not None:
+        current_node = current_node.next
+
+    new_node = SinglyLinkedListNode(data)
+    current_node.next = new_node
+
+    return head
